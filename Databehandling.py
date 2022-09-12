79bc150770f82468bc5e5d466f65f9fb4c2d88c3
David <davidripollokholm@hotmail.com>
2022-09-12 16:12:32 +0200

    er ikke blevet færdig Har fået nogen problemer med at lave csv filen til lister, skal nok kigge åp det igen

--- Databehandling.py
+++ Databehandling.py
@@ -1,22 +1,42 @@
 
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize
+from pip._internal.utils import virtualenv
 from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 from scipy.misc import electrocardiogram
 from scipy.signal import find_peaks
-import plotly.graph_objects as go
+
 import os
+import csv
+import re
 
-# assign directory
-directory = 'CSMHUNT_12301_2022-9-7_14-23-3.cvs'
 
-# iterate over files in that directory
+def conv(s):
+  try:
+    s = float(s)
+  except:
+    pass
+  return s
 
-s = open('CSMHUNT_12301_2022-9-7_14-23-3.cvs').read()
-for i in s:
-    print(i)
-print(s)
+# Denne her kode aflæser og fordeler dataet i ders baretemte label.
 
+with open('./CSMHUNT_12301_2022-9-7_14-23-3.cvs', 'r') as file:
+  csvreader = csv.reader(file, delimiter=';')
+  var = []
+  for i in csvreader:
+    t = [conv(s) for s in i]
+      var.append(t)
+  data =  zip(var)
+  data = [_ for _ in zip(data[0],data[1:])]
+  #print(data)
+  print(data,'y')
+  #plt.plot(data[0,1],data[9,1])
+
+
+#  data = [int(x) for x in data]
+ # print(data)
+
+ # [conv(s) for s in data]
+  #plt.plot(data[1,1],data[9,1])
 
