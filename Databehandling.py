41c6e2866fd4694f0d6c688da43569efb07cdf69
David <davidripollokholm@hotmail.com>
2022-09-12 13:19:56 +0200

    Forsøg på at plotte dataet

---
+++ Databehandling.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize
+from scipy.signal import argrelextrema
+import matplotlib.pyplot as plt
+from scipy.misc import electrocardiogram
+from scipy.signal import find_peaks
+import plotly.graph_objects as go
+import os
+
+# assign directory
+directory = 'CSMHUNT_12301_2022-9-7_14-23-3.cvs'
+
+# iterate over files in that directory
+print(directory)
+
