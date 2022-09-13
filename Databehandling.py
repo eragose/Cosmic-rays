b60f2d4fd318c5cc33b284c82eef2b95a7269c3c
David <davidripollokholm@hotmail.com>
2022-09-13 07:18:30 +0200

    er ikke blevet færdig Jeg har sorteret en liste og plottet dem det er det sidste plot kan dog ikke huske hvordan man laver at poisson fordeling

--- Databehandling.py
+++ Databehandling.py
@@ -1,11 +1,13 @@
 
 import numpy as np
+import pylab as pl
 import scipy.optimize
 from pip._internal.utils import virtualenv
 from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 from scipy.misc import electrocardiogram
 from scipy.signal import find_peaks
+from scipy.stats import poisson
 
 import os
 import csv
@@ -27,16 +29,76 @@ with open('./CSMHUNT_12301_2022-9-7_14-23-3.cvs', 'r') as file:
   for i in csvreader:
     t = [conv(s) for s in i]
       var.append(t)
-  data =  zip(var)
-  data = [_ for _ in zip(data[0],data[1:])]
+  data = list(zip(*var))
   #print(data)
-  print(data,'y')
-  #plt.plot(data[0,1],data[9,1])
+  # Nu plotter jeg dataet
 
+
+x,y = data[1][1:],data[9][1:]
+
+
+fig, ax = plt.subplots()
+
+
+ax.stem(x,y )
+
+ax.set(xlim=(0, 8), xticks=np.arange(-1, len(x)+2),
+       ylim=(0, 1000), yticks=np.linspace(0, 1100, 10))
+
+plt.show()
+
+  print(x)
 
 #  data = [int(x) for x in data]
  # print(data)
 
  # [conv(s) for s in data]
   #plt.plot(data[1,1],data[9,1])
+
+  #%%
+
+with open('./CSMHUNT_12301_2022-9-7_16-3-44.cvs', 'r') as file:
+  csvreader = csv.reader(file, delimiter=';')
+  var = []
+  for i in csvreader:
+    t = [conv(s) for s in i]
+      var.append(t)
+  data = list(zip(*var))
+  #print(data)
+  # Nu plotter jeg dataet
+
+
+x,y = data[0][1:],data[9][1:]
+
+
+fig, ax = plt.subplots()
+
+
+ax.stem(x,y )
+
+ax.set(xlim=(0, len(x)), xticks=np.arange(0,len(x),20),
+       ylim=(0, 1000), yticks=np.linspace(0, 1100, 10))
+
+plt.show()
+
+  print(x)
+mu = np.mean(y)
+print(mu)
+y = sorted(y)
+print(y)
+
+fig, ax = plt.subplots()
+
+ax.stem(x,y )
+
+ax.set(xlim=(0, len(x)), xticks=np.arange(0,len(x)+2,20),
+       ylim=(0, 1000), yticks=np.linspace(0, 1100, 10))
+
+plt.show()
+
+  print(x)
+#%%
+plt.hist(x,density=True, edgecolor='black')
+plt.show()
+
 
