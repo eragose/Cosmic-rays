8eb80878539eb488daf42c4724f0260c05e96b9d
eragose <erik.o.linder@gmail.com>
2022-09-19 12:12:02 +0200

    problem fix?

--- Databehandling_vinkler.py
+++ Databehandling_vinkler.py
@@ -16,6 +16,15 @@ import csv
 
 import re
 
+plt.rc("font", family=["Helvetica", "Arial"]) # skifter skrifttype
+plt.rc("axes", labelsize=16)   # skriftstørrelse af `xlabel` og `ylabel`
+plt.rc("xtick", labelsize=14, top=True, direction="in")  # skriftstørrelse af ticks og viser ticks øverst
+plt.rc("ytick", labelsize=14, right=True, direction="in")
+plt.rc("axes", titlesize=16)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(6,5,forward=True)
+
 
 def conv(s):
   try:
@@ -45,9 +54,9 @@ x = np.linspace(0,90,7)
 coefficients = np.polyfit(x, y, 3)
 fx = np.linspace(-1, 100, 100)
 fy = np.polyval(coefficients, fx)
-plt.plot(fx, fy, '-')
-plt.plot(x, y, 'ro')
-plt.show()
+#plt.plot(fx, fy, '-')
+#plt.plot(x, y, 'ro')
+#plt.show()
 
 
 
@@ -55,9 +64,8 @@ x_mu = np.mean(x)
 
 #y = np.array()
 
-fig, ax = plt.subplots()
 
-ax.plot(x,y)
+#ax.plot(x,y)
 
 #plt.show()
 
@@ -74,13 +82,13 @@ def funlin(x, a, b,c):
 
 yler = np.array((y)**0.5)
 
-plt.errorbar(x, y, yler, fmt='o', ms=6, capsize=3)
+#plt.errorbar(x, y, yler, fmt='o', ms=6, capsize=3)
 
 pinit1 = np.array([1, 120.,0])
-xhelp1 = np.linspace(0.,100.,90)
+xhelp1 = np.linspace(0.,90.,90)
 yhelp1 = funlin(xhelp1, pinit1[0], pinit1[1], pinit1[2])
-plt.plot(xhelp1, yhelp1, 'r.')
-plt.show()
+#plt.plot(xhelp1, yhelp1, 'r.')
+#plt.show()
 
 #%%
 popt, pcov = curve_fit(funlin, x, y, p0=pinit1, sigma=yler, absolute_sigma=True)
@@ -90,7 +98,12 @@ print('usikkerheder:',perr)
 chmin = np.sum(((y-funlin(x, *popt))/yler)**2)
 print('chi2:',chmin,' ---> p:', ss.chi2.cdf(chmin,4))
 
-plt.errorbar(x, y, yler, fmt="o", ms=6, capsize= 3)
-plt.plot(xhelp1, funlin(xhelp1, *popt), 'k-.')
-plt.show()
+ax.errorbar(x, y, yler, fmt="o", ms=6, capsize= 3, label = "data")
+ax.plot(xhelp1, funlin(xhelp1, *popt), 'k-.', label = "fit")
+ax.legend()
+ax.set_ylabel("Counts")
+ax.set_xlabel("Angle (degree)")
+ax.set_xticks(ticks = np.linspace(0, 90, 7))
+ax.set_title("Mc shizzle")
+fig.show()
 
